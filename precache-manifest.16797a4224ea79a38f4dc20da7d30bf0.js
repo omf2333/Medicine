90216b3d0afb3812553bfeed7c9e50b9bc7f9bf9
omf2333 <1923208364@qq.com>
2019-07-12 01:23:29 +0800

    Updates

---
+++ precache-manifest.16797a4224ea79a38f4dc20da7d30bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bffd35b08bf1008e544680410c3ecf1",
+    "url": "/Medicine/index.html"
+  },
+  {
+    "revision": "c2975d136d478e8ee532",
+    "url": "/Medicine/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "2db9ab353dd60d409f2d",
+    "url": "/Medicine/static/css/main.83fc215f.chunk.css"
+  },
+  {
+    "revision": "c2975d136d478e8ee532",
+    "url": "/Medicine/static/js/2.83cb1457.chunk.js"
+  },
+  {
+    "revision": "2db9ab353dd60d409f2d",
+    "url": "/Medicine/static/js/main.6b93f8a7.chunk.js"
+  },
+  {
+    "revision": "e4a0248ee3249c8082cc",
+    "url": "/Medicine/static/js/runtime~main.cf0222a3.js"
+  }
+]);
